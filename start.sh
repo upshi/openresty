2b16ef7669b9d9bc499d6d22fb115f102dfdb94d
upshi <zjuwyn@gmail.com>
2018-08-27 00:05:48 +0800

    hello world openresty

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-/opt/openresty/bin/openresty -p `pwd`
+sudo /opt/openresty/bin/openresty -p `pwd`
---
+++ stop.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+sudo /opt/openresty/bin/openresty -s stop
